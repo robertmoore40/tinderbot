f0b1aa907957bbf2bfceb140b217af1ba4139b4c
robertmoore40 <robertcmoore92@gmail.com>
2020-08-24 10:08:37 -0400

    added self login

--- tinder_bot.py
+++ tinder_bot.py
@@ -6,3 +6,6 @@ from secrets import username, password
 class TinderBot():
     def __init__(self):
          self.driver = webdriver.Chrome()
+
+     def login(self):
+        self.driver.get('https://tinder.com')
