cbdeb71de98b5c1f8ebaa5bff7dfe3659e1bd24a
robertmoore40 <robertcmoore92@gmail.com>
2020-08-24 10:13:50 -0400

    changed formatting

--- tinder_bot.py
+++ tinder_bot.py
@@ -15,4 +15,4 @@ class TinderBot():
         fb_btn = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/div[2]/button')
         fb_btn.click()
 
-         base_window = self.driver.window_handles[0]
+        base_window = self.driver.window_handles[0]
